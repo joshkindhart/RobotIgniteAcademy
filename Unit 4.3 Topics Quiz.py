d31898a61015c2c5af2166be3a0d59ded83bfe0c
= <=>
2023-01-22 16:40:02 +0000

    rewrote with working OOP

--- Unit 4.3 Topics Quiz.py
+++ Unit 4.3 Topics Quiz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 # -----------------------------------------------------------------------------------------
-
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
@@ -10,89 +9,79 @@ from sensor_msgs.msg import LaserScan
 
 
 class Robot():
-    def __init__(self, msg):
-        self.move = Twist()
-        self.velocity = 0.1
-        self.time = 5
+    def __init__(self):
+        self.msg = None
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.sub = rospy.Subscriber(
+            '/kobuki/laser/scan', LaserScan, self.callback)
+        self.move = Twist()
+        self.linear_velocity = 0.0
+        self.angular_velocity = 0.0
+        
+    def callback(self, msg):
+        self.msg = msg
 
-    def should_proceed(self, msg):
-        if msg.ranges[359] > 1.0 or msg.ranges[359] == float('inf'):
-            print(msg.ranges[359], "I am good to proceed! ")
-            return True
+        if self.should_proceed():
+            self.move_forward()
         else:
-            print('Something is in my way \n')
-            return False
+            self.stop()
+            self.turn(self.choose_direction())
+        if self.should_proceed():
+            self.stop_turning()
+            self.move_forward()
+        print(self.msg.ranges[719],
+              self.msg.ranges[359], self.msg.ranges[0])
+        self.pub.publish(self.move)
 
     def move_forward(self):
-        self.move.linear.x = self.velocity
-        print('Proceeding! \n')
+        self.linear_velocity = 0.25
+        self.move.linear.x = self.linear_velocity
 
-    def is_advancing(self):
-        if self.move.linear.x > 0.0:
+    def stop(self):
+        self.linear_velocity = 0.0
+        self.move.linear.x = self.linear_velocity
+
+    def turn_left(self):
+        self.angular_velocity = 3
+        self.move.angular.z = self.angular_velocity
+
+    def turn_right(self):
+        self.angular_velocity = -3
+        self.move.angular.z = self.angular_velocity
+
+    def stop_turning(self):
+        self.angular_velocity = 0.0
+        self.move.angular.z = self.angular_velocity
+
+    def should_proceed(self):
+        if self.msg.ranges[359] > 1:
             return True
         else:
             return False
 
-    def stop(self):
-        self.move.linear.x = 0.0
-        print("Stopping \n")
-
-    def choose_direction(self, msg, direction=None):
-        for i in msg.ranges[0:240]:
-            for j in msg.ranges[480:719]:
-                if i > j:
+    def choose_direction(self, direction=None):
+        for i in self.msg.ranges[0:358]:
+            for j in self.msg.ranges[360:719]:
+                if i < 1 and j > 1:
+                    direction = 'left'
+                elif i > 1 and j < 1:
                     direction = 'right'
-                elif i < j:
+                else:
                     direction = 'left'
         return direction
 
-    def turn(self, direction):
+    def turn(self, var):
+        if var is not None:
+            direction = var
         if direction == 'left':
-            self.velocity = 0.1
-            self.move.angular.z = self.velocity
-            print('Turning {robot.choose_direction}! \n')
+            self.move.angular.z += 0.1
         elif direction == 'right':
-            self.velocity = -0.1
-            self.move.angular.z = self.velocity
-            print('Turning {robot.choose_direction}! \n')
-
-# -----------------------------------------------------------------------------------------
-
-
-def robot_controller(msg):
-    robot = Robot(msg)
-    if robot.should_proceed(msg):
-        robot.move_forward()
-        robot.pub.publish(robot.move)
-    else:
-        robot.stop()
-        robot.pub.publish(robot.move)
-
-    if not robot.is_advancing():
-        robot.turn(robot.choose_direction)
-        robot.pub.publish(robot.move)
-
-
-# -----------------------------------------------------------------------------------------
-
-
-def callback(msg):
-    robot_controller(msg)
-
-
-def listener():
-    rospy.init_node('topics_quiz')
-    sub = rospy.Subscriber('/kobuki/laser/scan/', LaserScan, callback)
-    rospy.spin()
-
-# -----------------------------------------------------------------------------------------
+            self.move.angular.z = -0.1
+        else:
+            direction = 'left'
 
 
 if __name__ == '__main__':
-    while not rospy.is_shutdown():
-        try:
-            listener()
-        except rospy.ROSInterruptException:
-            pass
-        break
+    rospy.init_node('topics_quiz_node')
+    robot = Robot()
+    rospy.spin()
